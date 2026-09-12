ab538510c85ffbeb7a9a71d1c01b871b5c699d88
Priyadarshini <dharshinirai@gmail.com>
2026-09-12 12:39:24 +0530

    edit

--- Locators.java
+++ Locators.java
@@ -19,7 +19,6 @@ driver.findElement(By.name("inputPassword")).sendKeys("hello123");
 driver.findElement(By.className("signInBtn")).click();
 System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 driver.findElement(By.linkText("Forgot your password?")).click();
-Thread.sleep(1000);//
 driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
 driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@rsa.com");
 driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
